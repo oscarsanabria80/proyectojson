d2c9da31a04d4d3af5f88729043bf66a33b6e2f9
oscarsanabria80 <oscar.poncedeleonsanabria80@gmail.com>
2022-03-01 20:11:14 +0100

    Programa principal y funciones modificado, en proceso de mejoras

--- funciones.py
+++ funciones.py
@@ -1,7 +1,7 @@
 import json
 
 
-# .Listas todos los productos que contenga la tienda
+# 1.Listas todos los productos que contenga la tienda
 def listar_productos(tienda):
     lista=[]
     contar= tienda.get("products")
@@ -10,7 +10,7 @@ def listar_productos(tienda):
             lista.append(i.get("name"))
     return lista
 
-#introducir el nombre de un cliente y contar los productos comprado.
+#2.introducir el nombre de un cliente y contar los productos comprado.
 def clientes_productos(nombre,doc):
     lista1=[]
     for cliente in doc["users"]:
@@ -21,3 +21,39 @@ def clientes_productos(nombre,doc):
             print("CLIENTE NO REGISTRADO EN LA BASE DE DATO.")
     return lista1
         
+#3.Mostrar el producto que contenga el precio que se ha introducido por teclado.
+
+def precio_productos(precio):
+    lista=[]
+    pre= float(input("Intoduce un precio:  "))
+    productos=precio.get("products")
+    for p in productos:
+        if p["price"]== pre:
+            lista.append(p.get("name"))
+    return lista
+
+
+#4.Pedir por teclado el nombre de un cliente y un dia (day1,day2,day3) y que muestres el color del producto comprado ese día.
+
+def buscar_productos(dia,nombre,doc):
+    lista=[]
+    nombre=input("Introduce el nombre:  ")
+    dia=input("Un dia:  ")
+    docc= doc.get("users")
+    for i in docc:
+        if i["name"]== nombre:
+    return lista
+#5. Mostrar el nombre del producto y sus ventas, con el precio más alto del que indiquemos.
+
+def precio_alto(precio,doc):
+    lista=[]
+    lista3=[]
+    precio=float(input("Precio estimado: "))
+    do= doc.get("products")
+    for c in do:
+        if c["price"] < precio:
+            #lista.append(c.get("name"))
+            lista3.append(c.get("sold"))
+
+    return lista3
+
--- programaprincipal.py
+++ programaprincipal.py
@@ -1,15 +1,20 @@
-from ctypes.wintypes import 
-from http import client
-import json
+import json 
 from funciones import *
 #leer json
 with open("tiendadeproducto.json") as fichero:
     doc=json.load(fichero)
 
-#listar produductos
+#1.Listas todos los productos que contenga la tienda
 print(listar_productos(doc))
 
-#introducir el nombre de un cliente y contar los productos comprado.
-nombre=input("introduce un nombre de clientes: ")
-for calcular in clientes_productos(nombre,doc):
-    print(calcular)
+#2.introducir el nombre de un cliente y contar los productos comprado.
+for calcular in clientes_productos(doc):
+ print(calcular)
+
+#3.Mostrar el producto que contenga el precio que se ha introducido por teclado.
+print(precio_productos(doc))
+
+
+#4.Pedir por teclado el nombre de un cliente y un dia (day1,day2,day3) y que muestres el color del producto comprado ese día.
+for calcularr in buscar_productos(doc):
+    print(calcularr)
