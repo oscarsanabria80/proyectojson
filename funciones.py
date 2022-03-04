fe2b8b733b6e05ec3cfa008dbce85f5bc1437051
oscarsanabria80 <oscar.poncedeleonsanabria80@gmail.com>
2022-03-04 16:16:02 +0100

    Ficheros programaprincipal y funciones actualizado con correcciones

--- funciones.py
+++ funciones.py
@@ -3,57 +3,59 @@ import json
 
 # 1.Listas todos los productos que contenga la tienda
 def listar_productos(tienda):
-    lista=[]
-    contar= tienda.get("products")
-    for i in contar:
+    for i in tienda["products"]:
         if i.get("name"):
-            lista.append(i.get("name"))
-    return lista
+            print(i["name"])
+    return print(i["name"])
 
 #2.introducir el nombre de un cliente y contar los productos comprado.
-def clientes_productos(nombre,doc):
+def clientes_productos(doc):
     lista1=[]
+    nombre=input("introduce un nombre:  ")
     for cliente in doc["users"]:
         if cliente["name"]==nombre:
             lista1.append(cliente.get("name"))
-            lista1.append(cliente.get("purchase"))
-        if not cliente["name"]==nombre:
-            print("CLIENTE NO REGISTRADO EN LA BASE DE DATO.")
+            lista1.append(len(cliente.get("purchase")))
+
     return lista1
         
 #3.Mostrar el producto que contenga el precio que se ha introducido por teclado.
 
 def precio_productos(precio):
-    lista=[]
     pre= float(input("Intoduce un precio:  "))
     productos=precio.get("products")
     for p in productos:
         if p["price"]== pre:
-            lista.append(p.get("name"))
-    return lista
-
+            print(p.get("name"))
+    return print(p.get("name"))
 
 #4.Pedir por teclado el nombre de un cliente y un dia (day1,day2,day3) y que muestres el color del producto comprado ese día.
 
-def buscar_productos(dia,nombre,doc):
-    lista=[]
+def buscar_productos(tienda):
+    listaproductos=[]
     nombre=input("Introduce el nombre:  ")
     dia=input("Un dia:  ")
-    docc= doc.get("users")
-    for i in docc:
-        if i["name"]== nombre:
-    return lista
+    for e in tienda["users"]:
+        if e["name"]==nombre:
+            compra=e["purchase"]
+            for dias in compra:
+                arti=dias["day"]
+                for d in arti:
+                    if d["dia"]==dia:
+
+                        
+                        listaproductos.append(d["color"])
+            
+
+    return listaproductos
+
 #5. Mostrar el nombre del producto y sus ventas, con el precio más alto del que indiquemos.
 
-def precio_alto(precio,doc):
-    lista=[]
+def producto_precio(tiend):
     lista3=[]
-    precio=float(input("Precio estimado: "))
-    do= doc.get("products")
-    for c in do:
-        if c["price"] < precio:
-            #lista.append(c.get("name"))
-            lista3.append(c.get("sold"))
-
+    precio=float(input("Precio estimado:  "))
+    for p in tiend["products"]:
+        if p["price"]>precio:
+            lista3.append(p["name"])
+            lista3.append(p["sold"])
     return lista3
-
--- programaprincipal.py
+++ programaprincipal.py
@@ -1,20 +1,50 @@
-import json 
+import json
 from funciones import *
+
 #leer json
 with open("tiendadeproducto.json") as fichero:
     doc=json.load(fichero)
 
+print(''' 
+1.Listas todos los productos que contenga la tienda
+2.introducir el nombre de un cliente y contar los productos comprado. 
+3.Mostrar el producto que contenga el precio que se ha introducido por teclado.
+4.Pedir por teclado el nombre de un cliente y un dia (day1,day2,day3) y que muestres el color del producto comprado ese día.
+5.Mostrar el nombre del producto y sus ventas, con el precio más alto del que indiquemos.
+6.Salir''')
+
+print("\n\n")
+
+
+menu=int(input("Introduce una de las opciones que se le indica:  "))
+
+print("\n\n")
+while menu !=6:
+
 #1.Listas todos los productos que contenga la tienda
-print(listar_productos(doc))
+    if menu==1:
+        print(listar_productos(doc))
 
 #2.introducir el nombre de un cliente y contar los productos comprado.
-for calcular in clientes_productos(doc):
- print(calcular)
+    if menu==2:
+        for calcular in clientes_productos(doc):
+            print(calcular)
 
 #3.Mostrar el producto que contenga el precio que se ha introducido por teclado.
-print(precio_productos(doc))
+    if menu==3:
+        print(precio_productos(doc))
 
 
 #4.Pedir por teclado el nombre de un cliente y un dia (day1,day2,day3) y que muestres el color del producto comprado ese día.
-for calcularr in buscar_productos(doc):
-    print(calcularr)
+    if menu==4:
+        for calcularr in buscar_productos(doc):
+                print("\n\n")
+                print("Color del producto es:  ",calcularr)
+                print("\n\n")
+
+#5. Mostrar el nombre del producto y sus ventas, con el precio más alto del que indiquemos.
+    if menu==5:
+        for hola in producto_precio(doc):
+            print(doc["name"])
+
+    menu=int(input("Introduce una de las opciones que se le indica:  "))
